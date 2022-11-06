9e4290c071ef0c5c5343fa3ec3a4c5c14fff0629
Vaibhav2157 <92290672+Vaibhav2157@users.noreply.github.com>
2022-11-06 23:32:58 +0530

    initial commit

---
+++ admin/src/pages/new/New.jsx
@@ -0,0 +1,80 @@
+import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+import { useState } from "react";
+import axios from "axios";
+import FileBase from "react-file-base64";
+
+
+const New = ({ inputs, title }) => {
+ 
+  const [info, setInfo] = useState({});
+
+  const handleChange = (e) => {
+    setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    await axios.post("/auth/register", info); 
+  };
+
+  console.log(info);
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={
+                info.img
+                  ? info.img
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                </label>
+                <FileBase
+              type="file"
+              multiple={false}
+              onDone={({ base64 }) =>
+                setInfo(
+                  {...info,img:base64}
+                )
+              }
+            />
+              </div>
+
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input
+                    onChange={handleChange}
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    id={input.id}
+                  />
+                </div>
+              ))}
+              <button onClick={handleClick}>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default New;
